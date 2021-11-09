a11fefaed2f5299489f5fcc47e256073bffe387d
Jacklyn Malone <jjmlmhc.09@gmail.com>
2021-11-09 15:33:00 -0500

    added html routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,3 +1,25 @@
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = 3001;
+
+// app.use(express.static('public'));
+// HTML routes
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`app listening at http://localhost:${PORT}`)
+);
