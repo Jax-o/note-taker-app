68f52ec57accd1a632662d84b7730b8417d2c214
Jacklyn Malone <jjmlmhc.09@gmail.com>
2021-11-10 22:35:31 -0500

    trying to deploy to heroku changed file paths

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ app.use(express.static('public'));
 
 
 app.get('/', (req, res) =>
-	res.sendFile(path.join(__dirname, 'public/index.html'))
+	res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-	res.sendFile(path.join(__dirname, 'public/notes.html'))
+	res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 
@@ -88,7 +88,7 @@ app.post('/api/notes', (req, res) => {
 
 
 app.get('*', (req, res) =>
-	res.sendFile(path.join(__dirname, 'public/index.html'))
+	res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.listen(PORT, () =>
